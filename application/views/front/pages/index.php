fef254e3668e8f1d501fabef8c62e5c9b7123e88
PiotrRybarczyk-php <piotr.rybarczyk1994@gmail.com>
2021-09-14 12:31:53 +0200

    work

--- application/views/front/pages/index.php
+++ application/views/front/pages/index.php
@@ -44,30 +44,72 @@
             <div>
                 <h1>NOWY QASHQAI</h1>
                 <h2>Jedyny taki crossover</h2>
-                <button>Dostępne Modele<i><img style="width:11px;" src="<?= base_url('assets/front/icons/arrow-white.svg'); ?>"></i></button>
+                <button>Dostępne Modele<i></i></button>
             </div>
         </div>
-        <div class="grid-4 gmob-1" id="cargrid">
+        <div class="grid-4 gmob-2" id="cargrid">
             <div class="tile_outline ">
-                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)"></div>
+                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)">
+                    <div>
+                        <h1>Juke</h1>
+                        <h2>Crossover Coupe</h2>
+                        <span class="flex_box flex_align_e" style="height:65%;"><button><i></i></button></span>
+                    </div>
+                </div>
             </div>
             <div class="tile_outline">
-                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)"></div>
+                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)">
+                    <div>
+                        <h1>Juke</h1>
+                        <h2>Crossover Coupe</h2>
+                        <span class="flex_box flex_align_e" style="height:65%;"><button><i></i></button></span>
+                    </div>
+                </div>
             </div>
             <div class="tile_outline">
-                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)"></div>
+                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)">
+                    <div>
+                        <h1>Juke</h1>
+                        <h2>Crossover Coupe</h2>
+                        <span class="flex_box flex_align_e" style="height:65%;"><button><i></i></button></span>
+                    </div>
+                </div>
             </div>
             <div class="tile_outline">
-                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)"></div>
+                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)">
+                    <div>
+                        <h1>Juke</h1>
+                        <h2>Crossover Coupe</h2>
+                        <span class="flex_box flex_align_e" style="height:65%;"><button><i></i></button></span>
+                    </div>
+                </div>
             </div>
             <div class="tile_outline flex_align_e">
-                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)"></div>
+                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)">
+                    <div>
+                        <h1>Juke</h1>
+                        <h2>Crossover Coupe</h2>
+                        <span class="flex_box flex_align_e" style="height:65%;"><button><i></i></button></span>
+                    </div>
+                </div>
             </div>
             <div class="tile_outline flex_align_e">
-                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)"></div>
+                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)">
+                    <div>
+                        <h1>Juke</h1>
+                        <h2>Crossover Coupe</h2>
+                        <span class="flex_box flex_align_e" style="height:65%;"><button><i></i></button></span>
+                    </div>
+                </div>
             </div>
             <div class="tile_outline flex_align_e">
-                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)"></div>
+                <div class="small_tile" style="background-image:url(<?= base_url() . 'assets/front/img/car-1.jpeg'; ?>)">
+                    <div>
+                        <h1>Juke</h1>
+                        <h2>Crossover Coupe</h2>
+                        <span class="flex_box flex_align_e" style="height:65%;"><button><i></i></button></span>
+                    </div>
+                </div>
             </div>
             <div class="tile_outline flex_align_e">
                 <div class="small_tile last_tile">
@@ -75,6 +117,131 @@
                     <button class="btn_black">dostępne od ręki <i><img style="width:11px;" src="<?= base_url('assets/front/icons/arrow-red.svg'); ?>"></i></button>
                 </div>
             </div>
+        </div>
+    </div>
+</section>
+<section class="sep_top-xl">
+    <div class="block_normal yama_padding">
+        <h2>NAGRODY GRUPY YAMA</h2>
+        <h1>Dziękujemy za uznanie!</h1>
+    </div>
+    <div class="grid-3 gtab-2 gmob-1 timeline yama_padding">
+        <div class="timeline_box">
+            <div>
+                <year>2021</year>
+                <h3>SALON OSIELSKO</h3>
+                <subdiv>
+                    <img src="<?= base_url()  . 'assets/front/icons/trophy-star.svg'; ?>"></img>
+                    <label>
+                        <h5>Laureat konkursu</h5>
+                        <h4>Orły Motoryzacji</h4>
+                    </label>
+                </subdiv>
+                <h3>SALON TORUŃ</h3>
+                <subdiv>
+                    <img src="<?= base_url()  . 'assets/front/icons/trophy-star.svg'; ?>"></img>
+                    <label>
+                        <h5>Laureat konkursu</h5>
+                        <h4>Orły Motoryzacji</h4>
+                    </label>
+                </subdiv>
+            </div>
+        </div>
+        <div class="timeline_box">
+            <div>
+                <year>2020</year>
+                <h3>SALON OSIELSKO</h3>
+                <subdiv>
+                    <img src="<?= base_url()  . 'assets/front/icons/trophy-star.svg'; ?>"></img>
+                    <label>
+                        <h5>Laureat konkursu</h5>
+                        <h4>Orły Motoryzacji</h4>
+                    </label>
+                </subdiv>
+                <h3>SALON TORUŃ</h3>
+                <subdiv>
+                    <img src="<?= base_url()  . 'assets/front/icons/trophy-star.svg'; ?>"></img>
+                    <label>
+                        <h5>Laureat konkursu</h5>
+                        <h4>Orły Motoryzacji</h4>
+                    </label>
+                </subdiv>
+            </div>
+        </div>
+        <div class="timeline_box">
+            <div>
+                <year>2019</year>
+                <h3>SALON OSIELSKO</h3>
+                <subdiv>
+                    <img src="<?= base_url()  . 'assets/front/icons/trophy-one.svg'; ?>"></img>
+                    <label>
+                        <h5>Nr 1 w Polsce</h5>
+                        <h4>Wielki Test Salonów</h4>
+                    </label>
+                </subdiv>
+                <subdiv>
+                    <img src="<?= base_url()  . 'assets/front/icons/trophy-star.svg'; ?>"></img>
+                    <label>
+                        <h5>Laureat konkursu</h5>
+                        <h4>Orły Motoryzacji</h4>
+                    </label>
+                </subdiv>
+                <h3>SALON TORUŃ</h3>
+                <subdiv>
+                    <img src="<?= base_url()  . 'assets/front/icons/trophy-star.svg'; ?>"></img>
+                    <label>
+                        <h5>Laureat konkursu</h5>
+                        <h4>Orły Motoryzacji</h4>
+                    </label>
+                </subdiv>
+            </div>
+        </div>
+        <div class="timeline_box">
+            <div>
+                <year>2018</year>
+            </div>
+        </div>
+        <div class="timeline_box">
+            <div>
+                <year>2017</year>
+            </div>
+        </div>
+        <div class="timeline_box">
+            <div>
+                <year>2016</year>
+            </div>
+        </div>
+        <div class="timeline_box">
+            <div>
+                <year>2015</year>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sep_top-m">
+    <div class="flex_box yama_padding" id="infoflex">
+        <div class="info_left">
+            <img src="<?= base_url() . 'assets/front/icons/menu_logo.svg' ?>"></img>
+            <h1>Wartości naszej marki</h1>
+        </div>
+        <div class="info_right">
+            <h1>NASI KLIENCI</h1>
+            <h2>Ich potrzeby, komfort, zaufanie - tworzymy miejsce gdzie wydarza się wszystko</h2>
+            <h1>NISSAN</h1>
+            <h2>Samochód, który z dumą i pasją sprzedajemy</h2>
+            <h1>YAMA</h1>
+            <h2>Marka dealerska, której logo to symbol naszych doświadczeń i wartości</h2>
+        </div>
+    </div>
+</section>
+<section class="sep_top-xl">
+    <div class="listing_flex">
+        <div class="listing_bc" style="background-image:url(<?= base_url() . 'assets/front/img/Osielsko/info-1.jpg' ?>)"></div>
+        <div class="listing_info">
+            <h2>UWAŻNOŚĆ - MAMY TO W DNA</h2>
+            <h1>Historia marki Yama</h1>
+            <desc>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacinia sed ligula a lacinia. Aenean id neque volutpat, finibus risus non, facilisis nisl. Donec scelerisque libero quis sem varius vulputate. In bibendum dolor odio, sed ornare nunc suscipit in. Suspendisse tempor posuere justo sit amet luctus. Nunc sit amet erat efficitur leo mollis congue. Maecenas ultrices, ante sit amet pellentesque luctus, est velit tempus odio, a facilisis justo dolor a sem. Cras vel ante purus. Cras lectus dui, blandit nec volutpat eget, tempus tristique ipsum. Aenean euismod est id lacus sagittis luctus eget.</desc>
+            <button>O NAS</button>
         </div>
     </div>
 </section>
